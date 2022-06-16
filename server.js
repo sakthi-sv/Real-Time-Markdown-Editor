6d4ec1f540927fa56cd3b0df2c2b5531c4fcd7c3
Sakthivel <sakthivelssvj@gmail.com>
2022-06-16 21:57:21 +0530

    Markdown Editor

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.render("pad");
+});
+
+app.get("/:id", function (req, res) {
+  res.render("pad");
+});
+
+// var sharejs = require("share");
+// set up redis server
+// var redisClient;
+// console.log(process.env.REDISTOGO_URL);
+// redisClient = require("redis");
+
+var options = {
+  db: { type: 'redis' }
+};
+
+// sharejs.server.attach(app, options);
+
+var port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log("Server listining to port 8000");
+});
